63b93e14de9430a3c7f4e9f4a0dfec84d637a2b9
CelestialEcho <rockstar.gtav1488@gmail.com>
2025-02-09 21:42:42 +0200

    added winSize() method

--- src/Render/JIT_Render/JIT_Render.cpp
+++ src/Render/JIT_Render/JIT_Render.cpp
@@ -20,22 +20,10 @@ BOOL JIT_Render::addLine(mathclass::VoidVec2d pos1, mathclass::VoidVec2d pos2, c
     {
         this->SetCurPos(mathclass::VoidVec2d(pos1.x, pos1.y));
         std::cout << filler;
-
-        if (pos1.x == pos2.x && pos1.y == pos2.y)
-            break;
-
+        if (pos1.x == pos2.x && pos1.y == pos2.y) break;
         int e2 = 2 * err;
-
-        if (e2 > -dy)
-        {
-            err -= dy;
-            pos1.x += sx;
-        }
-        if (e2 < dx)
-        {
-            err += dx;
-            pos1.y += sy;
-        }
+        if (e2 > -dy) { err -= dy;  pos1.x += sx; }
+        if (e2 < dx) { err += dx; pos1.y += sy; }
     }
     return true;
 }
@@ -48,6 +36,21 @@ BOOL JIT_Render::addRect(mathclass::VoidVec2d pos1, mathclass::VoidVec2d pos2, c
     bool side4 = addLine(mathclass::VoidVec2d(pos1.x, pos2.y), pos1, filler);
 
     return (side1 && side2 && side3 && side4);
+}
+
+mathclass::VoidVec2d winSize() // will return (-1, -1) if there is an error
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+        return mathclass::VoidVec2d(csbi.srWindow.Right - csbi.srWindow.Left + 1, csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
+    }
+//                                x     y
+    return mathclass::VoidVec2d(-1.f, -1.f);
+}
+
+BOOL JIT_Render::addEllipse(mathclass::VoidVec2d pos, mathclass::VoidVec2d size, float radius_minor, float radius_major)
+{
+    
 }
 
 
--- src/Render/JIT_Render/JIT_Render.h
+++ src/Render/JIT_Render/JIT_Render.h
@@ -12,14 +12,15 @@ class JIT_Render
 {
 public:
     JIT_Render() {}
+    mathclass::VoidVec2d size();
     BOOL addLine(mathclass::VoidVec2d pos1, mathclass::VoidVec2d pos2, char filler);
-    BOOL addRect(mathclass::VoidVec2d pos1, mathclass::VoidVec2d pos2, char filler);  
+    BOOL addRect(mathclass::VoidVec2d pos1, mathclass::VoidVec2d pos2, char filler);
+    BOOL addEllipse(mathclass::VoidVec2d pos, mathclass::VoidVec2d size, float radius_minor, float radius_major);
 
 
 private:
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD coords;
-
     BOOL SetCurPos(mathclass::VoidVec2d vec);
     
 };
